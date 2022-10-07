64386764a0bf3559c4697ecb468f2b209a4c25f4
RedPho <medasdan@gmail.com>
2022-10-07 14:33:42 +0300

    Add bookCard variable in js

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let bookCard =  '<div class="col-sm-4">' +
+                  '<div class="card">' +
+                    '<div class="card-body">' +
+                      '<h5 class="card-title">Book title</h5>' +
+                        '<span class="card-text">Author:</span>' +
+                          '<p class="card-text">AuthorName</p>' +
+                          '<span class="card-text">Number of pages:</span>' +
+                          '<p class="card-text">NumberOfPages</p>' +
+                          '<button class="btn btn-info">Readed: No</button>' +
+                          '<button class="btn btn-danger">Delete</button>' +
+                    '</div>' +
+                  '</div>' +
+                '</div>'
